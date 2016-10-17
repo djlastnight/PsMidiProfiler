5b7ade3691f160afd149be7052f75bef55723fd6
djlastnight <djlastnight@gmail.com>
2016-10-17 23:33:30 +0300

    Updated Custom Controller monitor descriptions

--- PsMidiProfiler/Enums/DeviceType.cs
+++ PsMidiProfiler/Enums/DeviceType.cs
@@ -1,27 +1,29 @@
-﻿using System.ComponentModel;
-namespace PsMidiProfiler.Enums
+﻿namespace PsMidiProfiler.Enums
 {
+    using System.ComponentModel;
+
     public enum DeviceType : int
     {
-        [Description("Drums device type. Used for 4/5/7 lane drums and real drums with hi-hat pedal (non CC4).")]
+        [Description("Drums device type. Used for 4/5/7 lane MIDI drums. Combine with Method 0.")]
         Drums = 0,
 
-        [Description("Guitar device type. Used for 5 fret guitars.")]
+        [Description("Guitar device type. Used for 5 fret MIDI guitars. Combine with Methods 0, 4 or 5")]
         Guitar = 1,
 
-        [Description("Gamepad device type. Used to profile xInput and direct input gamepads.")]
+        [Description("Gamepad device type. Used to profile your MIDI device as gamepad. Combine with Method 0.")]
         Gamepad = 2,
 
         [Description("Computer keyboard device type.")]
         Keyboard = 3,
 
-        [Description("Piano device type. The game supports 5-88 keys piano. It is recommended to use 5,13,25 or 88 keys.")]
+        [Description("Piano device type. The game supports 5-88 keys piano. It is recommended to use 5,13,25 or 88 keys piano. " +
+            "Combine with method 1.")]
         Piano = 4,
 
-        [Description("Real drums device type. Used for real drums with CC4 hi-hat pedal support.")]
+        [Description("Real drums device type. Used for real drums with hi-hat pedal. Combine with Methods 0 or 3")]
         DrumsReal = 5,
 
-        [Description("Real guitar device type. Used for Rock Band SysEx based real guitars.")]
+        [Description("Real guitar device type. Used for Rock Band SysEx based real guitars. Combine with method 2.")]
         GuitarReal = 6,
 
         [Description("Dance pad device type. Used to profile dance pad controllers.")]
--- PsMidiProfiler/Enums/Method.cs
+++ PsMidiProfiler/Enums/Method.cs
@@ -1,24 +1,26 @@
-﻿using System.ComponentModel;
-namespace PsMidiProfiler.Enums
+﻿namespace PsMidiProfiler.Enums
 {
+    using System.ComponentModel;
+
     public enum Method
     {
-        [Description("The default method. Listens for note on and note off events only.")]
+        [Description("Method 0 - The default method. Listens for note on and note off events only.")]
         Manual = 0,
 
-        [Description("Standard MIDI keyboard, which pitch wheel is bound to whammy")]
+        [Description("Method 1 - Standard MIDI keyboard with pitch wheel bound to whammy. Buttons are not required.")]
         Piano = 1,
 
-        [Description("Rock Band pro guitar, which is using system exclusive (SysEx) MIDI messages.")]
+        [Description("Method 2 - Rock Band pro guitar, which is using system exclusive (SysEx) MIDI messages. Buttons are not required.")]
         GuitarSysEx = 2,
 
-        [Description("Real drums with HiHat pedal, which reports its state as midi Control Change 4 (36) MIDI Message")]
+        [Description("Method 3 - Real drums with HiHat pedal, which reports its position as Control Change 4 MIDI Message. " + 
+        "Phase Shift recognizes 3 hi-hat states depending on message velocity: Open (0-79), Sizzle (80-120), Closed (121-127).")]
         DrumsCC4HiHat = 3,
 
-        [Description("Standard MIDI Guitar")]
+        [Description("Method 4 - Standard MIDI Guitar.")]
         GuitarMidi = 4,
 
-        [Description("MIDI Guitar, which pitch wheel is bound to whammy")]
+        [Description("Method 5 - Legacy MIDI Guitar - Pitch wheel bound to whammy.")]
         GuitarLegacy = 5,
     }
 }
